5ed61a0f86d939b42874ab45427106b8c83ace8a
Matthew Sklar <matthewsklar227@gmail.com>
2018-04-25 18:10:05 -0400

    Replace request with urllib.request

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import requests
+import urllib.request as request
 import json
 
-response = requests.get("https://api.wunderground.com/api/43b25c75d7f17987/conditions/q/GA/Atlanta.json").json()
+response = json.loads((request
+                       .urlopen("https://api.wunderground.com/api/43b25c75d7f17987/conditions/q/GA/Atlanta.json")
+                       .read()
+                       .decode("utf-8")))
 
 with open('atlanta.json', 'w') as file:
     json.dump(response, file, indent=4)
